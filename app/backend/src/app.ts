d44308aeed990a089f247d5c68e0b8639e344bce
Gerardo Lima <gerardo.lima@gmail.com>
2025-11-16 00:09:20 +0000

    feat(app): add infrastructure for validation and serialization

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,7 +1,12 @@
 import Fastify from 'fastify'
 import {userRoutes} from './modules/user/user.routes.ts'
+import {serializerCompiler, validatorCompiler} from 'fastify-type-provider-zod'
 
 const app = Fastify({logger: true})
+
+// enables payload validation and response serialization using Zod
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 // register routes (prefix: /api)
 app.register(userRoutes, {prefix: '/api/users'})
